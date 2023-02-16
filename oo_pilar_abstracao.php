11e3587df7187a600d12aac72e214a770dedd43d
GiovannyMarin <giovanny466@hotmail.com>
2023-02-16 17:46:33 -0300

    Start PHP POO Course

---
+++ oo_pilar_abstracao.php
@@ -0,0 +1,26 @@
+<?php
+
+//modelo
+
+class Funcionario
+{
+    //atributos
+    public $nome = null;
+    public $telefone = null;
+    public $numFilhos = null;
+
+    //metodos
+
+    function resumirCadFunc()
+    {
+        return 'esse eh o resumo do cadastro do funcionario';
+    }
+
+    function modificarNumFilhos()
+    {
+        //afetar atributo de um obj
+
+    }
+};
+
+?><!-->
